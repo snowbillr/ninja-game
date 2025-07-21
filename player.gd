923ea73bd2337657418ceac0fbc7977888b7f6e1
Bill Reed <bill.reed@babylist.com>
2025-07-21 15:50:30 -0400

    jump action

--- player.gd
+++ player.gd
@@ -8,8 +8,9 @@ extends CharacterBody2D
 @export var friction_coefficient: float = 0.25
 @export var acceleration_coefficient: float = 0.25
 @export var gravity_coefficient: float = 0.03
+@export var jump_force: float = 350.0
 
-enum state { IDLE, RUNNING, FALLING }
+enum state { IDLE, RUNNING, JUMPING, FALLING }
 
 var state_machine = state.IDLE
 
@@ -23,7 +24,10 @@ func _physics_process(delta: float) -> void:
 		state.IDLE:
 			animation_player.play("idle")
 			
-			if x_input != 0:
+			if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+				velocity.y = -jump_force
+				state_machine = state.JUMPING
+			elif x_input != 0:
 				state_machine = state.RUNNING
 				sprite_2d.flip_h = true if sign(x_input) == -1 else false
 			elif not is_on_floor():
@@ -35,18 +39,43 @@ func _physics_process(delta: float) -> void:
 		state.RUNNING:
 			animation_player.play("run")
 		
-			if not is_on_floor():
+			if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+				velocity.y = -jump_force
+				state_machine = state.JUMPING
+			elif not is_on_floor():
 				state_machine = state.FALLING
 			elif x_input == 0:
 				state_machine = state.IDLE
 			else:
-				self.velocity.x = lerp(self.velocity.x, speed * sign(x_input), 0.25)
+				sprite_2d.flip_h = true if sign(x_input) == -1 else false
+				self.velocity.x = lerp(self.velocity.x, speed * sign(x_input), acceleration_coefficient)
 			
 			move_and_slide()
+		state.JUMPING:
+			animation_player.play("jump")
+			
+			if velocity.y > 0:
+				state_machine = state.FALLING
+			
+			if x_input != 0:
+				sprite_2d.flip_h = true if sign(x_input) == -1 else false
+				self.velocity.x = lerp(self.velocity.x, speed * sign(x_input), acceleration_coefficient)
+			else:
+				self.velocity.x = lerp(self.velocity.x, 0.0, friction_coefficient)
+
+			velocity.y = lerp(self.velocity.y, max_gravity, gravity_coefficient)
+			move_and_slide()
 		state.FALLING:
+			animation_player.play("fall")
+			
 			if is_on_floor():
 				state_machine = state.IDLE
 			else:
+				if x_input != 0:
+					sprite_2d.flip_h = true if sign(x_input) == -1 else false
+					self.velocity.x = lerp(self.velocity.x, speed * sign(x_input), acceleration_coefficient)
+				else:
+					self.velocity.x = lerp(self.velocity.x, 0.0, friction_coefficient)
 				velocity.y = lerp(self.velocity.y, max_gravity, gravity_coefficient)
 				
 			move_and_slide()
