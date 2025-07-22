8023a6322a054472d88f8cc49b8c4420d1561ee6
Bill Reed <bill.reed@babylist.com>
2025-07-21 22:18:26 -0400

    add script template for gsmstate

---
+++ addons/gsm/script_templates/GSMState/gsm_state.gd
@@ -0,0 +1,17 @@
+extends GSMState
+
+
+func _ready():
+	super()
+
+# Called when a state is entered
+func _enter(args: Dictionary) -> void:
+	pass
+
+# Called when a state is exited	
+func _exit() -> void:
+	pass
+
+# TBD
+func _check_transitions():
+	pass
