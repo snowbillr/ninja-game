2b6067a972bbb7c3adc239fdc04fd21add951c01
Bill Reed <bill.reed@babylist.com>
2025-07-24 14:46:13 -0400

    double jump

--- scenes/player/states/air.gd
+++ scenes/player/states/air.gd
@@ -1,17 +1,31 @@
 extends PlayerState
 
 var do_jump := false
+var remaining_movements := {
+	"jumps": 0
+}
 
 func _enter(args: Dictionary) -> void:
-	if args.has("do_jump"):
-		do_jump = args.do_jump
-
-func _physics_process(_delta: float) -> void:
-	if self.player.velocity.y < 0:
+	do_jump = args.has("do_jump") and args.do_jump
+	
+	if do_jump:
 		self.player.animation_player.play("jump")
 	else:
 		self.player.animation_player.play("fall")
+	
+	remaining_movements.jumps = 1
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump"):
+		if remaining_movements.jumps > 0:
+			remaining_movements.jumps -= 1
+			self.player.velocity.y = -self.player.player_stats.jump_force
+			self.player.animation_player.play("double_jump")
+
+func _physics_process(_delta: float) -> void:
+	if self.player.velocity.y > 0:
+		self.player.animation_player.play("fall")
+	
 	var x_input = Input.get_axis("move_left", "move_right")
 	if x_input != 0:
 		self.player.sprite_2d.flip_h = true if sign(x_input) == -1 else false
