d8a1cfcb622a4dd9b1758603ee39b7b51239f198
Bill Reed <bill.reed@babylist.com>
2025-07-31 22:33:36 -0400

    wip - figuring out ground_attack state vs animationtree

---
+++ scenes/player/resources/attack_data.gd
@@ -0,0 +1,4 @@
+class_name AttackData
+extends Resource
+
+@export var animation_name: String
---
+++ scenes/player/resources/combo_attack.gd
@@ -0,0 +1,8 @@
+class_name ComboAttack
+extends Resource
+
+@export var attack: AttackData
+
+@export var next_attack: ComboAttack
+@export var next_up_attack: ComboAttack
+@export var next_down_attack: ComboAttack
