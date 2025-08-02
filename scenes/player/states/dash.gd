98aea0d9dda31dc7dde2b06ad85033354b30c3e3
Bill Reed <bill.reed@babylist.com>
2025-08-02 19:37:21 -0400

    small tweaks

--- scenes/player/states/dash.gd
+++ scenes/player/states/dash.gd
@@ -9,10 +9,10 @@ func _ready():
 
 func _enter(_args: Dictionary):
 	super (_args)
+
 	if not self.player.is_on_floor():
 		self.player.air_movement_charges.consume_dash()
 	
-
 	self.player.velocity.x = (1 if not self.player.sprite_2d.flip_h else -1) * self.player.player_stats.dash_speed
 	self.player.velocity.y = 0
 	
